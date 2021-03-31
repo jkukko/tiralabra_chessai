dba67c32c595f230dc5de81048c77d78b6fdf5ad
jkukko <joonas.kukkonen@gmail.com>
2021-04-01 01:06:02 +0300

    ChessBoard init

---
+++ src/main/java/datastructureproject/ChessBoard.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastructureproject;
+
+/**
+ *
+ * @author kukkojoo
+ */
+public class ChessBoard {
+    private int[][] board;
+    
+    public ChessBoard() {
+        this.board = new int[9][9];
+    }
+
+    public int[][] getBoard() {
+        return this.board;
+    }
+    
+    
+    
+    
+}
