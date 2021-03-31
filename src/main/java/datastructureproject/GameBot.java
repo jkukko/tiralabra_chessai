48e994a79419a609e6ecf34c9f4d3d9279f6efb7
jkukko <joonas.kukkonen@gmail.com>
2021-04-01 00:57:47 +0300

    GameBot init

---
+++ src/main/java/datastructureproject/GameBot.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastructureproject;
+
+/**
+ *
+ * @author kukkojoo
+ */
+
+import chess.bot.ChessBot;
+import chess.engine.GameState;
+
+public class GameBot implements ChessBot {
+
+    @Override
+    public String nextMove(GameState gamestate) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
