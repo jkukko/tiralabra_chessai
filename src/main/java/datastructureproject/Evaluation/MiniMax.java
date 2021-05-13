2658faed97a7e044f9fa31b616eceb4a0f2f4f62
jkukko <joonas.kukkonen@gmail.com>
2021-05-13 15:31:27 +0300

    Minimax logic

--- src/main/java/datastructureproject/Evaluation/MiniMax.java
+++ src/main/java/datastructureproject/Evaluation/MiniMax.java
@@ -9,6 +9,7 @@ import datastructureproject.ChessBoard;
 import datastructureproject.ChessLogic;
 import datastructureproject.Move;
 import java.util.List;
+import datastructureproject.OwnStructures.OwnList;
 
 
 /**
@@ -27,18 +28,27 @@ public class MiniMax {
         this.orginalBoard = board;
     }
     
+    /**
+     * Returns best move for player in specific chessboard 
+     * @param board given chessboard
+     * @param depth move many moves ahead is calculated
+     * @param player for which player best move is given
+     * @return best move
+     */
+    
     public Move getBestMove(ChessBoard board, int depth, int player) {
-        List<Move> moves = this.logic.legalMoves(board, 1);
+        OwnList moves = this.logic.legalMoves(board, player);
         ChessBoard testBoard = new ChessBoard();
         testBoard.initBoard();
         testBoard.setBoard(board);
         Move bestMove = null;
         
-        int minValue = -99999;
-        int maxValue = 99999;
+        int minValue = 99999;
+        int maxValue = -99999;
         int value;
-        
-        for (Move move: moves) {
+     
+        for (int i = 0; i < moves.size(); i++) {
+            Move move = (Move) moves.get(i);
             int eaten = testBoard.movePieceAndGetNewPositionValue(move);
             
             if (player == 1) {
@@ -50,10 +60,10 @@ public class MiniMax {
             System.out.println(move.getOldCoordinate().getCoordinate() + move.getNewCoordinate().getCoordinate() + ": " + value + " " + eaten);
             testBoard.undoMoveWithOldValue(move, eaten);
             
-            if (player == 1 && value <= maxValue) {
+            if (player == 1 && value >= maxValue) {
                 maxValue = value;
                 bestMove = move;
-            } else if (player == 2 && value >= minValue) {
+            } else if (player == 2 && value <= minValue) {
                 minValue = value;
                 bestMove = move;                
             }
@@ -67,6 +77,13 @@ public class MiniMax {
     private boolean whiteTurn() {
         return true;
     }
+    
+    /**
+     * Returns max value of specific chessboard that can be achieved
+     * @param board specific chessboard
+     * @param depth how many moves forward
+     * @return biggest value
+     */
 
     private int max(ChessBoard board, int depth) {
         
@@ -74,24 +91,35 @@ public class MiniMax {
             return evaluation.evalueation(board, 1);
         }
         
-        List<Move> moves = this.logic.legalMoves(board, 1);
+        OwnList moves = this.logic.legalMoves(board, 2);
         
         int maxValue = -99999;
         
-        for (Move move: moves) {
+        for (int i = 0; i < moves.size(); i++) {
+            Move move = (Move) moves.get(i);
             //board.movePiece(move);
+            System.out.println(move.getMove());
             int eaten = board.movePieceAndGetNewPositionValue(move);
             int otherSideScore = min(board, (depth - 1));
             //board.undoMove(move);
             board.undoMoveWithOldValue(move, eaten);
-            if(otherSideScore >= maxValue) {
+            if(otherSideScore > maxValue) {
                 maxValue = otherSideScore;
             }
             
         }
         
+        System.out.println(maxValue);
+        
         return maxValue;
     }
+    
+    /**
+     * Returns min value of specific chessboard that can be achieved
+     * @param board specific chessboard
+     * @param depth how many moves forward
+     * @return smallest value
+     */
 
     private int min(ChessBoard board, int depth) {
         
@@ -99,23 +127,23 @@ public class MiniMax {
             return evaluation.evalueation(board, 2);
         }
         
-        List<Move> moves = this.logic.legalMoves(board, 1);
+        OwnList moves = this.logic.legalMoves(board, 1);
         
         int minValue = 99999;
         
-        for (Move move: moves) {
+        for (int i = 0; i < moves.size(); i++) {
+            Move move = (Move) moves.get(i);
             //board.movePiece(move);
             int eaten = board.movePieceAndGetNewPositionValue(move);
             int otherSideScore = max(board, (depth - 1));
             //board.undoMove(move);
             board.undoMoveWithOldValue(move, eaten);
-            if (otherSideScore <= minValue) {
+            if (otherSideScore < minValue) {
                 minValue = otherSideScore;
             }
         }
         
         //System.out.println("MIN VALUE: " + minValue);
-        
         return minValue;
     }
     
